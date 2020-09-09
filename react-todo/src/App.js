8d11d4a4c96dc83f00978b81f544441f00e7e459
Kamil Blanik <kamil.lukasz.blanik@gmail.com>
2020-09-09 22:52:09 +0200

    Added generating todolist

--- react-todo/src/App.js
+++ react-todo/src/App.js
@@ -12,7 +12,7 @@ function App() {
         <h1>TODO List</h1>
       </header>
       <Form todos={todos} setTodos={setTodos} inputText={inputText} setInputText={setInputText}/>
-      <TodoList />
+      <TodoList todos={todos} setTodos={setTodos} />
     </div>
   );
 }
